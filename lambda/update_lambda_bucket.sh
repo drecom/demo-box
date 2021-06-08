06eceeb4d81cec45ee6c7be42539e46286b2e30f
zhai_yujia <yujia.zhai@drecom.co.jp>
2021-06-08 18:00:32 +0900

    fix var name for hands-on event

--- lambda/update_lambda_bucket.sh
+++ lambda/update_lambda_bucket.sh
@@ -3,15 +3,34 @@
 ASSET_DIR="assets"
 TEMP_DIR="/tmp"
 UPLOAD_REGION="ap-northeast-1"
-UPLOAD_BUCKET="drecom-lambda"
+UPLOAD_BUCKET="drecom-terraform-workshop"
 UPLOAD_FILE_ALL="lambda.zip"
 UPLOAD_FILE_MAIN="lambda-main.zip"
 UPLOAD_FILE_LIB="lambda-lib.zip"
 
+# set s3 path
+if [ $# -eq 2 ]; then
+  while getopts d: OPT
+  do
+    case $OPT in
+        d ) UPLOAD_PATH="$OPTARG"
+            echo "S3 bucket's full path set to [ s3://${UPLOAD_BUCKET}/${UPLOAD_PATH} ]" ;;
+        * ) echo "Usage: $0 [-d <s3 bucket's path>] "
+            exit 1 ;;
+    esac
+  done
+else
+  echo "Usage:"
+  echo "  $0 -d <s3 bucket's path>" 1>&2
+  echo "Like: $0 -d `whoami`-lambda"
+  echo "S3 bucket's full path need to match the <aws-s3-lambda-bucket-name> which outputted by terraform."
+  exit 1
+fi
+
 # check current aws profile name
 CURRENT_PROFILE=`aws configure list | grep profile | awk '{print $2}'`
-echo "Current aws profile is [ $CURRENT_PROFILE ] "
-echo "Is that OK with you ? yes/no"
+echo "And current AWS profile is [ $CURRENT_PROFILE ] "
+echo "Are they OK with you ? yes/no"
 read INPUT
 # get user confirm
 if [ -z $INPUT ] ; then
@@ -25,9 +44,9 @@ elif [ $INPUT = 'y' ] || [ $INPUT = 'yes' ] ; then
   zip -r ${TEMP_DIR}/${UPLOAD_FILE_MAIN} ./* > /dev/null
   
   # transfer to s3 bucket
-  aws s3 cp ${TEMP_DIR}/${UPLOAD_FILE_ALL}  s3://${UPLOAD_BUCKET}/${UPLOAD_FILE_ALL}  --region ${UPLOAD_REGION}
-  aws s3 cp ${TEMP_DIR}/${UPLOAD_FILE_MAIN} s3://${UPLOAD_BUCKET}/${UPLOAD_FILE_MAIN} --region ${UPLOAD_REGION}
-  aws s3 cp ${TEMP_DIR}/${UPLOAD_FILE_LIB}  s3://${UPLOAD_BUCKET}/${UPLOAD_FILE_LIB}  --region ${UPLOAD_REGION}
+  aws s3 cp ${TEMP_DIR}/${UPLOAD_FILE_ALL}  s3://${UPLOAD_BUCKET}/${UPLOAD_PATH}/${UPLOAD_FILE_ALL}  --region ${UPLOAD_REGION}
+  aws s3 cp ${TEMP_DIR}/${UPLOAD_FILE_MAIN} s3://${UPLOAD_BUCKET}/${UPLOAD_PATH}/${UPLOAD_FILE_MAIN} --region ${UPLOAD_REGION}
+  aws s3 cp ${TEMP_DIR}/${UPLOAD_FILE_LIB}  s3://${UPLOAD_BUCKET}/${UPLOAD_PATH}/${UPLOAD_FILE_LIB}  --region ${UPLOAD_REGION}
 
 else
   echo "Oops !"
